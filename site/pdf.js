f5b159051b3a799eb64326ed7438d8f8dc7ba743
bm36 <logistica@bm36importadora.com.br>
2026-09-10 17:21:00 -0300

    colacando informações da venda 8

--- site/pdf.js
+++ site/pdf.js
@@ -59,7 +59,7 @@ const right = 180;
         pedido.cliente_id ||
         '';
 
-
+const vendedor = pedido.usuario_nome;
     // ============================================================
     // FORMATADORES DO PDF
     // ============================================================
@@ -248,7 +248,7 @@ pdf.text('Evento: ' + (pedido.evento || 'Não informado'), 130, 50)
 
 pdf.setFont('helvetica', 'bold');
 pdf.setFontSize(9);
-pdf.text('Vendedor: ' + (pedido.usuario_name || 'Não informado'), 130, 54)
+pdf.text('Vendedor: ' + (pedido.vendedor || 'Não informado'), 130, 54)
 
 pdf.setFont('helvetica', 'bold');
 pdf.setFontSize(9);
