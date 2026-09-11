85020d2e6d3acc30644346af717e59272212ff53
bm36 <logistica@bm36importadora.com.br>
2026-09-11 14:09:42 -0300

    colocando codigo js tr 2

--- site/CadastroTransportadora.js
+++ site/CadastroTransportadora.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const abaNova = document.querySelector('[data-tab="nova"]');
 
     // Seções que serão trocadas
-    const secaoLista = document.getElementById("transportadorasLista");
-    const secaoNova = document.getElementById("transportadoraForm");
+   const abaLista = document.getElementById("tabTransportadorasCadastradas");
+const abaNova = document.getElementById("tabNovaTransportadora");
 
     // Clique em "Transportadoras cadastradas"
     abaLista.addEventListener("click", () => {
