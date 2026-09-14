a177c41bdf6a1b90383945e7710fae75928c9984
bm36 <logistica@bm36importadora.com.br>
2026-09-14 09:02:57 -0300

    arrumando titulos de totais que esta torto 2

--- site/pdf.js
+++ site/pdf.js
@@ -367,13 +367,13 @@ async function gerarPdfPedido(
 
     pdf.text(
         'VALOR DE DESCONTO',
-        100,
+        110,
         yTotais
     );
 
     pdf.text(
         'TOTAL DO PEDIDO',
-        148,
+        158,
         yTotais
     );
 
